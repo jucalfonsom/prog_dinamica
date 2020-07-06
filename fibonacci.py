9a4f8dfd62fa31647e5d893df813d83c9da2768a
Juan Alfonso M <juancamiloalfonso@gmail.com>
2020-07-05 20:51:07 -0500

    Cálculo fibonacci con programación dinámica

--- fibonacci.py
+++ fibonacci.py
@@ -2,11 +2,28 @@ def fibonacci_recursivo(n):
     if n == 0 or n == 1:
         return 1
     
+
     return fibonacci_recursivo(n - 1) + fibonacci_recursivo (n - 2)
 
 
+def fibonacci_dinamico(n, memo = {}):
+    if n == 0 or n == 1:
+        return 1
+    
+
+    try:
+        return memo[n]
+    except KeyError:
+        resultado = fibonacci_dinamico(n - 1, memo) + fibonacci_dinamico(n - 2, memo)
+        memo[n] = resultado
+
+        return resultado
+
+
+    
+
 if __name__ == "__main__":
     n = int(input('Escribe un número para calcular el fibonacci: '))
-    resultado = fibonacci_recursivo(n)
+    resultado = fibonacci_dinamico(n)
 
     print(f'El resultado es: {resultado}')
